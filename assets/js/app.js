828e5e59cb99eda3c60ffb7ed8d9f71dc4658d4b
Daniel Sauer <danielsauer2000@gmail.com>
2025-07-07 17:14:46 -0600

    Revisión y mejoras al código original

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,17 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');//agregue . a name para referirnos a la clase
+const $b = document.querySelector('.blog');//cambie # por . porque no es un id
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) {//agregue el async para poder usar el await
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json();//faltaba convertirlo a json
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  $n.textContent = `${data.name}`;//cambie las comillas simples por backticks
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
 }
 
 function handleError(err) {
